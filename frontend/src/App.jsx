2a7ac21706fc59785682a20abcfce46085c68989
Abhigit2504 <varmaabhivardhan@gmal.com>
2024-10-29 12:51:27 +0530

    Added bcryptjs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,11 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"
+    style={{
+      minHeight: "100vh",
+      background: "linear-gradient(to bottom right, white, pink)",
+    }}>
       <ToastContainer />
       <Navbar/>
       <SearchBar/>
